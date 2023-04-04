df251e3569b3e8aaaf93c321997408c0b1dc30d1
muhamadzade <muhamadzade@gmail.com>
2023-04-04 20:56:02 +0430

    phase5

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post100(request):
