c9408d60d675048d837da4298a794a8ccceca6fb
muhamadzade <muhamadzade@gmail.com>
2023-04-04 20:43:04 +0430

    phase4

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post100/', views.post100, name='post100'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,20 @@
+from .models import Post
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse
+
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def post100(request):
+    me = User.objects.get(username='admin')
+    for i in range(100, 200):
+        now = timezone.now()
+        title = f"Title {i}"
+        text = f"text {i}"
+        Post.objects.create(author=me, title=title, text=text).publish()
+    return HttpResponse('100 post is created')
+    # Post.save()
